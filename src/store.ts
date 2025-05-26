e788965eb780abde873c4c55ece3e524c363f5c4
Le Quang Trong Nghia <lqtnghia2602@gmail.com>
2025-05-26 07:35:00 +0700

    storage

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,36 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// Lấy state từ localStorage (nếu có)
+const loadState = () => {
+  try {
+    const savedState = localStorage.getItem("reduxState");
+    if (savedState === null) {
+      return undefined;
+    }
+    return JSON.parse(savedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+// Lưu state vào localStorage
+const saveState = (state: any) => {
+  try {
+    const serializedState = JSON.stringify(state); // Chuyển state thành chuỗi JSON
+    localStorage.setItem("reduxState", serializedState); // Lưu vào localStorage
+  } catch (err) {
+    console.error("Lỗi khi lưu state:", err);
+  }
+};
+
+// Tạo store
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+
+// Đăng ký lắng nghe thay đổi state và lưu vào localStorage
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
